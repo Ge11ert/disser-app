e0b7a99536036e130a9de857819b728dfaa9c4d7
a.abramyants <a.abramyants@fun-box.ru>
2020-06-02 00:28:41 +0300

    [Feature] Добавление действий для dev-режима.

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,29 @@
 import { app, BrowserWindow } from 'electron';
+import electronReload from 'electron-reload';
+import isDev from 'electron-is-dev';
 import path from 'path';
 
 const htmlFile = path.join(__dirname, '../index.html');
 let mainWindow: Electron.BrowserWindow | null;
 
+if (isDev) {
+  electronReload(__dirname);
+
+  // Получение пути файловой системы, где находится приложение
+  console.log(app.getAppPath());
+
+  // Получение путей к стандартным папкам файловой системы
+  console.log(app.getPath('desktop'));
+  console.log(app.getPath('music'));
+  console.log(app.getPath('temp'));
+  console.log(app.getPath('userData'));
+}
+
 function createWindow() {
   mainWindow = new BrowserWindow({
     height: 600,
     width: 800,
+    show: false,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     }
@@ -17,6 +33,12 @@ function createWindow() {
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
+
+  mainWindow.once('ready-to-show', () => {
+    if (mainWindow !== null) {
+      mainWindow.show();
+    }
+  });
 
   mainWindow.on('closed', () => {
     mainWindow = null;
