64e090b36dc7484adb58a6bfabeeddbba7a0d32a
a.abramyants <a.abramyants@fun-box.ru>
2020-06-29 23:59:49 +0300

    [Feature] Запуск файндера по матрице, созданной из введённых координат.

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import ElectronApp from './electron/server';
 import Grid from './pathfinding/core/grid';
 import AStartFinder from './pathfinding/finder/a-star-finder';
-import { DisserAppAPI, AirConditions } from './types/interfaces';
+import Geo from './geo';
 import { cell } from './constants/grid';
+
+import type { DisserAppAPI, AirConditions } from './types/interfaces';
 
 export default class DisserApp implements DisserAppAPI {
   private electronApp: ElectronApp;
@@ -10,8 +12,11 @@ export default class DisserApp implements DisserAppAPI {
   private finderMatrix: number[][] = [];
   private finderGrid: Grid|null = null;
 
+  private readonly geo: Geo;
+
   constructor(public settings: { electron: Record<string, any> }) {
     this.electronApp = new ElectronApp(settings.electron, this);
+    this.geo = new Geo();
   }
 
   startElectronApp() {
@@ -26,17 +31,39 @@ export default class DisserApp implements DisserAppAPI {
     this.finderGrid.setCellSize({ x: cell.H_SIZE, y: cell.V_SIZE });
   }
 
+  applyInitialGeoConditions(geoConditions: Record<string, string>) {
+    this.geo.applyStartAndFinalCoords(geoConditions);
+    this.geo.findDistanceBetweenStartAndEndPoints();
+    this.createGridFromGeoCoords();
+  }
+
   startFinder(): void|never {
+    if (!this.geo.isCoordsLoaded()) {
+      throw new Error('No coords for start and dest points');
+    }
+
     if (this.finderGrid === null) {
       throw new Error('No pathfinding grid specified');
     }
     const finder = new AStartFinder({
       allowDiagonal: true,
     });
-    // TODO: вводить стартовую и конечную точки
-    const path = finder.findPath(0, 0, 4, 7, this.finderGrid);
-    const result = `Из точки: [0,0].  В точку: [4, 7].  Найденный маршрут: ${JSON.stringify(path)}`;
+    const path = finder.findPath(0, 0, this.finderGrid.width -1, this.finderGrid.height - 1, this.finderGrid);
+    const result = `Из точки: [0,0].  В точку: [${this.finderGrid.width - 1}, ${this.finderGrid.height - 1}].  Найденный маршрут: ${JSON.stringify(path)}`;
     this.electronApp.sendToWindow(result);
+  }
+
+  createGridFromGeoCoords(): void {
+    const { distanceInMiles } = this.geo;
+
+    const roundedX = Math.round(distanceInMiles.x);
+    const roundedY = Math.round(distanceInMiles.y);
+
+    const maxCellsX = Math.ceil(roundedX / cell.H_SIZE);
+    const maxCellsY = Math.ceil(roundedY / cell.V_SIZE);
+
+    this.finderGrid = new Grid(maxCellsX, maxCellsY);
+    this.finderGrid.setCellSize({ x: cell.H_SIZE, y: cell.V_SIZE });
   }
 }
 
