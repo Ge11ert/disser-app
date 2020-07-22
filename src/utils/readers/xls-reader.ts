dd7460fa26a47667934edf9811a0ab5b7ee2a313
a.abramyants <a.abramyants@fun-box.ru>
2020-07-23 01:38:31 +0300

    [Feature] Возможность получить список листов excel-файла.

--- src/utils/readers/xls-reader.ts
+++ src/utils/readers/xls-reader.ts
@@ -1,4 +1,4 @@
-import readXlsxFile, { ParsedResult } from 'read-excel-file/node'
+import readXlsxFile, { ParsedResult, SheetParsingOptions } from 'read-excel-file/node'
 import path from 'path';
 
 export enum StatusCode {
@@ -12,15 +12,37 @@ export enum StatusCode {
 const permittedExtensions = ['.xlsx', '.xls'];
 
 export default class XlsReader {
-  read(pathToFile: string): Promise<{ status: StatusCode, result: ParsedResult }> {
+  read(pathToFile: string, opts?: SheetParsingOptions): Promise<{ status: StatusCode, result: ParsedResult }> {
     const validationResult = this.validate(pathToFile);
 
     if (validationResult !== StatusCode.OK) {
       return Promise.reject({ status: validationResult, result: [] });
     }
 
-    return readXlsxFile(pathToFile)
+    return readXlsxFile(pathToFile, opts)
       .then((rows: ParsedResult) => ({ status: StatusCode.OK, result: rows }))
+      .catch(error => {
+        console.log(error);
+        switch (error.code) {
+          case 'ENOENT':
+            return { status: StatusCode.NOT_FOUND, result: [] };
+          case 'EISDIR':
+            return { status: StatusCode.NOT_EXCEL, result: [] };
+          default:
+            return { status: StatusCode.INTERNAL_ERROR, result: [] };
+        }
+      });
+  }
+
+  getSheetsList(pathToFile: string): Promise<{ status: StatusCode, result: { name: string}[] }> {
+    const validationResult = this.validate(pathToFile);
+
+    if (validationResult !== StatusCode.OK) {
+      return Promise.reject({ status: validationResult, result: [] });
+    }
+
+    return readXlsxFile(pathToFile, { getSheets: true })
+      .then((rows: { name: string}[]) => ({ status: StatusCode.OK, result: rows }))
       .catch(error => {
         console.log(error);
         switch (error.code) {
