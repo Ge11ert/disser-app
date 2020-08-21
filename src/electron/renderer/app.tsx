0fbf05aeb8013a6944a6ef94ec9e00dcb891e365
a.abramyants <a.abramyants@fun-box.ru>
2020-08-22 01:08:05 +0300

    [Feature] Рефакторинг app.tsx.

--- src/electron/renderer/app.tsx
+++ src/electron/renderer/app.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
 
 import InitialConditions from './initial-conditions';
 import FileSelector from './file-selector';
@@ -15,11 +18,19 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="page">
-        <InitialConditions/>
-        <FileSelector/>
-        <PathfinderController/>
-      </div>
+      <Container className="page" maxWidth="lg">
+        <Typography variant="h2" component="h1" gutterBottom>
+          Disser App
+        </Typography>
+
+        <Box display="flex">
+          <Box component="main">
+            <InitialConditions/>
+            <FileSelector/>
+            <PathfinderController/>
+          </Box>
+        </Box>
+      </Container>
     )
   }
 }
