f03293e2894cfff4725daa7d7c9eef525c738ae8
a.abramyants <a.abramyants@fun-box.ru>
2020-08-22 02:40:09 +0300

    [Feature] Рефакторинг компонента для старта расчётов.

--- src/electron/renderer/pathfinder-controller.tsx
+++ src/electron/renderer/pathfinder-controller.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
 
 const PathfinderController = () => {
   const onClick = () => {
@@ -8,12 +11,30 @@ const PathfinderController = () => {
 
   return (
     <div className="pathfinder-controller">
-      <button
-        className="pathfinder-controller__start"
-        onClick={onClick}
+      <Typography
+        variant="h4"
+        gutterBottom
+        className="pathfinder-controller__title"
       >
-        Найти путь
-      </button>
+        Расчёт множества траекторий
+      </Typography>
+
+      <Typography
+        variant="subtitle1"
+        className="pathfinder-controller__subtitle"
+      >
+        Нажмите кнопку для расчета параметров множества существующих четырехмерных траекторий
+      </Typography>
+
+      <Box my={2}>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={onClick}
+        >
+          Найти траектории
+        </Button>
+      </Box>
     </div>
   );
 };
