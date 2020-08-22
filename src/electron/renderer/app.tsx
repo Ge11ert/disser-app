8041c601fec1a3087af8187a16a3dc9ffed491b6
a.abramyants <a.abramyants@fun-box.ru>
2020-08-22 11:15:45 +0300

    [Feature] Добавление заглушки компонента с шагами выполнения расчётов.

--- src/electron/renderer/app.tsx
+++ src/electron/renderer/app.tsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import Container from '@material-ui/core/Container';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
 
 import InitialConditions from './initial-conditions';
 import FileSelector from './file-selector';
 import PathfinderController from './pathfinder-controller';
+import AsideStepper from './aside-stepper';
 
 class App extends React.Component {
   componentDidMount() {
@@ -27,6 +29,14 @@ class App extends React.Component {
             <InitialConditions/>
             <FileSelector/>
             <PathfinderController/>
+          </Box>
+
+          <Box mr={3} ml={6}>
+            <Divider orientation="vertical"/>
+          </Box>
+
+          <Box component="aside">
+            <AsideStepper/>
           </Box>
         </Box>
       </Container>
