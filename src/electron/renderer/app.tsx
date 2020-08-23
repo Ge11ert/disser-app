9689e7fada6c1de6bb70be19400bd30bbbd0621d
a.abramyants <a.abramyants@fun-box.ru>
2020-08-24 01:24:40 +0300

    [Feature] Использование хэш-роутера вместо browser-router.

--- src/electron/renderer/app.tsx
+++ src/electron/renderer/app.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import Main from './main';
-import AirConditions from './air-conditions';
 
 const App = () => (
-  <BrowserRouter>
+  <HashRouter>
     <Switch>
-      <Route exact path="/air" component={AirConditions}/>
       <Route exact path="/" component={Main}/>
     </Switch>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 const root = document.getElementById('react-root');
