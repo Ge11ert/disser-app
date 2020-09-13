58ee4c9885c554b06ddd15b9a0318d3f62f3d39b
a.abramyants <a.abramyants@fun-box.ru>
2020-09-13 22:29:44 +0300

    [Feature] Добавление компонента для графика высоты.

---
+++ src/electron/renderer/altitude-chart.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import Chart from './chart';
+
+interface Props {
+  startAltitude: number;
+  endAltitude: number;
+  distance: {
+    climb: number,
+    cruise: number,
+    descent: number,
+  };
+}
+
+class AltitudeChart extends React.Component<Props, {}> {
+  render() {
+    const { startAltitude, endAltitude, distance } = this.props;
+    return (
+      <Chart
+        width={1200}
+        height={600}
+        dataSet={{
+          label: 'Зависимость высоты от дистанции',
+          data: [
+            {
+              x: 0,
+              y: startAltitude,
+            },
+            {
+              x: distance.climb,
+              y: endAltitude,
+            },
+            {
+              x: distance.climb + distance.cruise,
+              y: endAltitude,
+            },
+            {
+              x: distance.climb + distance.cruise + distance.descent,
+              y: startAltitude,
+            }
+          ],
+          showLine: true,
+          fill: false,
+          cubicInterpolationMode: 'monotone',
+          borderColor: '#999'
+        }}
+        xAxeOptions={{
+          scaleLabel: {
+            display: true,
+            labelString: 'Дистанция, nm',
+            fontColor: '#333',
+            fontSize: 15,
+            lineHeight: '24px',
+          },
+          ticks: {
+            fontColor: '#333',
+          }
+        }}
+        yAxeOptions={{
+          scaleLabel: {
+            display: true,
+            labelString: 'Высота, ft',
+            fontColor: '#333',
+            fontSize: 15,
+            lineHeight: '24px',
+          },
+          ticks: {
+            min: startAltitude - 2000,
+            max: endAltitude + 2000,
+            fontColor: '#333',
+          }
+        }}
+      />
+    )
+  }
+}
+
+export default AltitudeChart;
