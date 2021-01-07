e0dec0e15c20e9141ece867f1b5d601ba71e9730
a.abramyants <a.abramyants@fun-box.ru>
2021-01-07 16:42:59 +0300

    [Improvement] Отображение нескольких графиков высоты.

--- src/electron/renderer/altitude-chart.tsx
+++ src/electron/renderer/altitude-chart.tsx
@@ -2,51 +2,91 @@ import React from 'react';
 import Chart from './chart';
 
 interface Props {
-  startAltitude: number;
-  endAltitude: number;
-  distance: {
-    climb: number,
-    cruise: number,
-    descent: number,
-  };
+  dataSets: Record<'fuel'|'time'|'combined'|'rta', {
+    startAltitude: number;
+    endAltitude: number;
+    distance: {
+      climb: number,
+      cruise: number,
+      descent: number,
+    };
+  }>;
 }
 
 const HEIGHT_MARGIN = 2000;
 
+const colors: { [type: string]: { primary: string, dark: string } } = {
+  fuel: {
+    primary: '#1976d2',
+    dark: '#004ba0',
+  },
+  time: {
+    primary: '#43a047',
+    dark: '#00701a',
+  },
+  combined: {
+    primary: '#f4511e',
+    dark: '#b91400',
+  },
+  rta: {
+    primary: '#fbc02d',
+    dark: '#c49000',
+  },
+  defaultColor: {
+    primary: '#999',
+    dark: '#999',
+  },
+};
+
+const labels: Record<string, string> = {
+  fuel: 'Минимум топлива',
+  time: 'Минимум времени',
+  combined: 'Смешанный критерий',
+  rta: 'Минимум задержки прибытия',
+  defaultLabel: 'Зависимость высоты от дистанции',
+};
+
 class AltitudeChart extends React.Component<Props, {}> {
   render() {
-    const { startAltitude, endAltitude, distance } = this.props;
-    const minHeight = Math.min(startAltitude, endAltitude) - HEIGHT_MARGIN;
-    const maxHeight = Math.max(startAltitude, endAltitude) + HEIGHT_MARGIN;
+    const dataSets = this.props.dataSets;
+    const startAltitudes = Object.values(dataSets).map(set => set.startAltitude);
+    const endAltitudes = Object.values(dataSets).map(set => set.endAltitude);
+    const minHeight = Math.min(...startAltitudes, ...endAltitudes) - HEIGHT_MARGIN;
+    const maxHeight = Math.max(...startAltitudes, ...endAltitudes) + HEIGHT_MARGIN;
     return (
       <Chart
         width={1200}
         height={600}
-        dataSets={[{
-          label: 'Зависимость высоты от дистанции',
-          data: [
-            {
-              x: 0,
-              y: startAltitude,
-            },
-            {
-              x: distance.climb,
-              y: endAltitude,
-            },
-            {
-              x: distance.climb + distance.cruise,
-              y: endAltitude,
-            },
-            {
-              x: distance.climb + distance.cruise + distance.descent,
-              y: startAltitude,
-            }
-          ],
-          showLine: true,
-          fill: false,
-          cubicInterpolationMode: 'monotone',
-          borderColor: '#999'
-        }]}
+        dataSets={Object.entries(dataSets).map(([key, value]) => {
+          const useDifferentAppearance = Object.keys(dataSets).length > 1;
+          const chartColor = useDifferentAppearance ? (colors[key] || colors.defaultColor) : colors.defaultColor;
+          return ({
+            label: useDifferentAppearance ? labels[key] : labels.defaultLabel,
+            data: [
+              {
+                x: 0,
+                y: value.startAltitude,
+              },
+              {
+                x: value.distance.climb,
+                y: value.endAltitude,
+              },
+              {
+                x: value.distance.climb + value.distance.cruise,
+                y: value.endAltitude,
+              },
+              {
+                x: value.distance.climb + value.distance.cruise + value.distance.descent,
+                y: value.startAltitude,
+              }
+            ],
+            showLine: true,
+            fill: false,
+            cubicInterpolationMode: 'monotone',
+            borderColor: chartColor.primary,
+            backgroundColor: chartColor.dark,
+          });
+        })}
         xAxeOptions={{
           scaleLabel: {
             display: true,
